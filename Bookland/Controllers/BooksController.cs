f4274cb3440f2323adadc91644b50ad22c29d092
kasiakor <kasia.kordys@gmail.com>
2018-02-07 14:27:29 +0000

    Add initial Model,View,Controller.

---
+++ Bookland/Controllers/BooksController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bookland.Models;
+
+namespace Bookland.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: Books/Random
+        public ActionResult Random()
+        {
+            var book = new Book() { Name = "Eleni" };
+
+            return View(book);
+        }
+    }
+}
