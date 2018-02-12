5b1e84738d3a755c7ce820b825fa0c4efafa79f5
kasiakor <kasia.kordys@gmail.com>
2018-02-12 09:13:43 +0000

    Add bookset controller, model and view.

---
+++ Bookland/Controllers/BooksetController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bookland.Controllers
+{
+    public class BooksetController : Controller
+    {
+        // GET: Bookset
+        public ActionResult NewBook()
+        {
+            return View();
+        }
+    }
+}
---
+++ Bookland/Models/Bookset.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bookland.Models
+{
+    public class Bookset
+    {
+        public string AuthorsFirstName { get; set; }
+        public string AuthorsLastName { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Language { get; set; }
+
+    }
+}
+
