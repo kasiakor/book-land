0b1bad0427e1196f30d979f1eabd808ffdcd202e
kasiakor <kasia.kordys@gmail.com>
2018-02-13 10:33:22 +0000

    Add sample MVC View

--- Bookland/Controllers/HomeController.cs
+++ Bookland/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace Bookland.Controllers
             return View();
         }
 
+        public ActionResult GoToHome ()
+        {
+            return View("MyHomePage");
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
