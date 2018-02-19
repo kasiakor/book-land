60bd7c4b0c5ae22daae41b1f08d190eb752feaa8
kasiakor <kasia.kordys@gmail.com>
2018-02-19 11:59:53 +0000

    Add new properties to Bookset and an xml file with test data

--- Bookland/Models/Bookset.cs
+++ Bookland/Models/Bookset.cs
@@ -7,11 +7,13 @@ namespace Bookland.Models
 {
     public class Bookset
     {
-        public string AuthorsFirstName { get; set; }
-        public string AuthorsLastName { get; set; }
+        public string ID { get; set; }
+        public string Author { get; set; }
         public string Title { get; set; }
         public string Genre { get; set; }
         public string Language { get; set; }
+        public int Price { get; set; }
+        public string Description { get; set; }
 
     }
 }
